00a986acedaa9f3feca8dde034fb948fa7fc74dc
bhu608@myy.haaga-helia.fi <bhu608@myy.haaga-helia.fi>
2026-02-27 11:09:13 +0200

    project initialization done for frontend and backend

---
+++ backend/playsembly/src/main/java/backend/playsembly/PlaysemblyApplication.java
@@ -0,0 +1,13 @@
+package backend.playsembly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaysemblyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaysemblyApplication.class, args);
+	}
+
+}
